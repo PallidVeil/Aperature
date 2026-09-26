dac0a2bbae72279c0b2548fc78b1a8778fb2f155
the lost <zbupbibop@gmail.com>
2026-09-26 10:22:36 +0300

    now it echoes the input character and solve some bugs on overwrite mode.

--- src/editor.c
+++ src/editor.c
@@ -3,12 +3,22 @@
 
 bool isInsertOn = true;
 
+void writeBufferToTerminal(char *buffer, Editor *editor)
+{
+    printf("\033[2K\r");
+    printf("%s", buffer);
+    printf("\033[%zuG", editor->cursor + 1);
+
+    fflush(stdout);
+}
+
 void moveCursorLeft(Editor *editor)
 {
     if (editor->cursor <= 0)
-    return;
+        return;
     editor->cursor--;
     printf("\033[D");
+    fflush(stdout);
 }
 
 void moveCursorRight(Editor *editor)
@@ -17,18 +27,21 @@ void moveCursorRight(Editor *editor)
         return;
     editor->cursor++;
     printf("\033[C");
+    fflush(stdout);
 }
 
 void moveCursorHome(Editor *editor)
 {
     editor->cursor = 0;
     printf("\033[%zuD", editor->cursor);
+    fflush(stdout);
 }
 
 void moveCursorEnd(Editor *editor)
 {
     editor->cursor = editor->length;
     printf("\033[%zuC", editor->length - editor->cursor);
+    fflush(stdout);
 }
 
 void EnterRawMode(struct termios *orig_termios)
@@ -46,112 +59,105 @@ void DisableRawMode(struct termios *orig_termios)
     tcsetattr(STDIN_FILENO, TCSAFLUSH, orig_termios);
 }
 
-void TerminalRenderer(char* buffer, Editor *editor)
+int executeEscapeSequence(InputType type, char *buffer, Editor *editor)
 {
-    printf("\033[2K\r");
-    printf("%s", buffer);
-    printf("\033[%zuG", editor->cursor + 1);
-}
-
-int executeEscapeSequence(InputType type,char* buffer, Editor *editor)
-{
-    switch(type)
+    switch (type)
     {
-        case INPUT_UP:
-            return 0;
-            break;
-        case INPUT_DOWN:
-            return 0;
-            break;
-        case INPUT_LEFT:
-            moveCursorLeft(editor);
-            return 0;
-            break;
-        case INPUT_RIGHT:
-            moveCursorRight(editor);
-            return 0;
-            break;
-        case INPUT_HOME:
-            moveCursorHome(editor);
-            return 0;
-            break;
-        case INPUT_END:
-            moveCursorEnd(editor);
-            return 0;
-            break;
-        case INPUT_INSERT:
-            isInsertOn = !isInsertOn;
-            setInsertOrOverwriteCursor();
-            return 0;
-            break;
-        case INPUT_DELETE:
-            editor->delete = true;
-            HandleInputMovement(buffer, editor);
-            editor->delete = false;
-            return 0;
-            break;
-        case INPUT_PAGE_UP:
-            return 0;
-            break;
-        case INPUT_PAGE_DOWN:
-            return 0;
-            break;
-        case INPUT_CONTROL_UP:
-            return 0;
-            break;
-        case INPUT_CONTROL_DOWN:
-            return 0;
-            break;
-        case INPUT_CONTROL_LEFT:
-            return 0;
-            break;
-        case INPUT_CONTROL_RIGHT:
-            return 0;
-            break;
-        case INPUT_UNKNOWN:
-            return 0;
-            break;
-        default:
-            return 0;
-            break;
+    case INPUT_UP:
+        return 0;
+        break;
+    case INPUT_DOWN:
+        return 0;
+        break;
+    case INPUT_LEFT:
+        moveCursorLeft(editor);
+        return 0;
+        break;
+    case INPUT_RIGHT:
+        moveCursorRight(editor);
+        return 0;
+        break;
+    case INPUT_HOME:
+        moveCursorHome(editor);
+        return 0;
+        break;
+    case INPUT_END:
+        moveCursorEnd(editor);
+        return 0;
+        break;
+    case INPUT_INSERT:
+        isInsertOn = !isInsertOn;
+        setInsertOrOverwriteCursor();
+        return 0;
+        break;
+    case INPUT_DELETE:
+        editor->delete = true;
+        HandleInputMovement(buffer, editor);
+        editor->delete = false;
+        return 0;
+        break;
+    case INPUT_PAGE_UP:
+        return 0;
+        break;
+    case INPUT_PAGE_DOWN:
+        return 0;
+        break;
+    case INPUT_CONTROL_UP:
+        return 0;
+        break;
+    case INPUT_CONTROL_DOWN:
+        return 0;
+        break;
+    case INPUT_CONTROL_LEFT:
+        return 0;
+        break;
+    case INPUT_CONTROL_RIGHT:
+        return 0;
+        break;
+    case INPUT_UNKNOWN:
+        return 0;
+        break;
+    default:
+        return 0;
+        break;
     }
 }
 
-InputType FindEscapeSequenceType(char* sequence)
+InputType FindEscapeSequenceType(char *sequence)
 {
-    if(strcmp(sequence, "[A") == 0)
+    if (strcmp(sequence, "[A") == 0)
         return INPUT_UP;
-    else if(strcmp(sequence, "[B") == 0)
+    else if (strcmp(sequence, "[B") == 0)
         return INPUT_DOWN;
-    else if(strcmp(sequence, "[C") == 0)
+    else if (strcmp(sequence, "[C") == 0)
         return INPUT_RIGHT;
-    else if(strcmp(sequence, "[D") == 0)
+    else if (strcmp(sequence, "[D") == 0)
         return INPUT_LEFT;
-    else if(strcmp(sequence, "[H") == 0)
+    else if (strcmp(sequence, "[H") == 0)
         return INPUT_HOME;
-    else if(strcmp(sequence, "[F") == 0)
+    else if (strcmp(sequence, "[F") == 0)
         return INPUT_END;
-    else if(strcmp(sequence, "[2~") == 0)
+    else if (strcmp(sequence, "[2~") == 0)
         return INPUT_INSERT;
-    else if(strcmp(sequence, "[3~") == 0)
+    else if (strcmp(sequence, "[3~") == 0)
         return INPUT_DELETE;
-    else if(strcmp(sequence, "[5~") == 0)
+    else if (strcmp(sequence, "[5~") == 0)
         return INPUT_PAGE_UP;
-    else if(strcmp(sequence, "[6~") == 0)
+    else if (strcmp(sequence, "[6~") == 0)
         return INPUT_PAGE_DOWN;
-    else if(strcmp(sequence, "[1;5A") == 0)
+    else if (strcmp(sequence, "[1;5A") == 0)
         return INPUT_CONTROL_UP;
-    else if(strcmp(sequence, "[1;5B") == 0)
+    else if (strcmp(sequence, "[1;5B") == 0)
         return INPUT_CONTROL_DOWN;
-    else if(strcmp(sequence, "[1;5C") == 0)
+    else if (strcmp(sequence, "[1;5C") == 0)
         return INPUT_CONTROL_RIGHT;
-    else if(strcmp(sequence, "[1;5D") == 0)
+    else if (strcmp(sequence, "[1;5D") == 0)
         return INPUT_CONTROL_LEFT;
     else
         return INPUT_UNKNOWN;
 }
 
-void HandleEscSequence(char* buffer, Editor *editor)
+void HandleEscSequence(char *buffer, Editor *editor)
 {
     char sequenceBuffer[MAX_ESCAPE_SEQUENCE_LENGTH] = {0};
     int i = 0;
@@ -166,17 +172,16 @@ void HandleEscSequence(char* buffer, Editor *editor)
             break;
         if (timeoutcheck == 0)
             break;
-        if (timeoutcheck == -1)// error add a check later
+        if (timeoutcheck == -1) // error add a check later
             break;
         read(STDIN_FILENO, &sequenceBuffer[i], 1);
         i++;
     }
     InputType type = FindEscapeSequenceType(sequenceBuffer);
-    executeEscapeSequence(type,buffer ,editor);
-
+    executeEscapeSequence(type, buffer, editor);
 }
 
-void HandleInputMovement(char* buffer, Editor *editor)
+void HandleInputMovement(char *buffer, Editor *editor)
 {
     int movingWay = 0;
     if (editor->delete)
@@ -187,7 +192,7 @@ void HandleInputMovement(char* buffer, Editor *editor)
             movingWay = -1;
         }
     }
-    else if (editor->input == 127) //backspace
+    else if (editor->input == 127) // backspace
     {
         if (editor->cursor > 0)
             movingWay = -1;
@@ -199,6 +204,9 @@ void HandleInputMovement(char* buffer, Editor *editor)
     else
     {
         buffer[(editor->cursor)] = editor->input;
+        if (buffer[(editor->cursor)] == '\0')
+            editor->length++;
+        writeBufferToTerminal(buffer, editor);
         moveCursorRight(editor);
     }
 
@@ -208,54 +216,55 @@ void HandleInputMovement(char* buffer, Editor *editor)
         {
             buffer[i] = buffer[i + 1];
         }
-    moveCursorLeft(editor);
-    editor->length--;
-    buffer[editor->length] = '\0';
+        moveCursorLeft(editor);
+        editor->length--;
+        buffer[editor->length] = '\0';
+        writeBufferToTerminal(buffer, editor);
     }
     else if (movingWay == 1)
     {
         editor->length++;
-        for(size_t i = editor->length; i > editor->cursor ; i--)
+        for (size_t i = editor->length; i > editor->cursor; i--)
         {
             buffer[i + 1] = buffer[i];
         }
         buffer[(editor->cursor)] = editor->input;
         moveCursorRight(editor);
         buffer[editor->length] = '\0';
+        writeBufferToTerminal(buffer, editor);
     }
 }
 
-
 InputType FindInputType(char input)
 {
-    switch (input) {
-        case 9:
-            return INPUT_TAB;
+    switch (input)
+    {
+    case 9:
+        return INPUT_TAB;
 
-        case 10:
-        case 13:
-            return INPUT_ENTER;
-        case 27:
-            return INPUT_ESCAPE;
-        case 127:
-            return INPUT_BACKSPACE;
-        default:
-            return INPUT_NORMAL;
+    case 10:
+    case 13:
+        return INPUT_ENTER;
+    case 27:
+        return INPUT_ESCAPE;
+    case 127:
+        return INPUT_BACKSPACE;
+    default:
+        return INPUT_NORMAL;
     }
-
 }
 
 void setInsertOrOverwriteCursor(void)
 {
     if (isInsertOn)
-        printf("\033[2 q");
-    else    
         printf("\033[6 q");
+    else
+        printf("\033[2 q");
 }
 
-int handleInputTypes(char* buffer, Editor *editor, InputType type)
+int handleInputTypes(char *buffer, Editor *editor, InputType type)
 {
-    switch(type)
+    switch (type)
     {
     case INPUT_NORMAL:
         HandleInputMovement(buffer, editor);
@@ -278,6 +287,6 @@ int handleInputTypes(char* buffer, Editor *editor, InputType type)
         HandleEscSequence(buffer, editor);
         return 0;
         break;
-}
-return 0;
+    }
+    return 0;
 }
